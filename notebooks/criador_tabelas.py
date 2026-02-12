02186fd1fc981155e6ad14c47c095d9964a096ba
luanzr4 <luanzragit@gmail.com>
2026-02-12 00:55:25 -0300

    variavel para peso de status de transacao adicionada

--- notebooks/criador_tabelas.py
+++ notebooks/criador_tabelas.py
@@ -1,4 +1,4 @@
-from random import choice,uniform
+from random import choice,uniform,choices
 from faker import Faker
 import pandas as pd
 
@@ -9,15 +9,25 @@ def criar_cliente(): #FUNCAO PARA CRIAR CLIENTES
     operacoes = ['PIX','TED','DOC']
     categorias = ['MERCADO','LAZER','CONTAS']
     status = ['CONCLUIDA','FALHA','PENDENTE']
+    pesos = [0.95 , 0.03 , 0.02]
     valor_transacao = uniform(100,10000) #RANDOMIZANDO O VALOR DA TRANSACAO ENTRE 100 E 10000 COM FLOAT
 
     return {
         'operacao':choice(operacoes), #ESCOLHENDO RANDOMICANTE AS OPCOES DA LISTA DE OPERACOES
         'categoria' : choice(categorias), #ESCOLHENDO RANDOMICANTE AS OPCOES DA LISTA DE OPERACOES
-        'status':choice(status), #ESCOLHENDO RANDOMICAMENTE AS OPCOES DA LISTA DE STATUS
+        'status':choices(status , weights=pesos, k=1)[0],
         'valor' : valor_transacao,
         'nome': fake.name() #CRIANDO NOMES ALEATORIOS BRASILEIROS
     }
+
+'''
+PARA SEGUIR UMA LINHA MAIS PROXIMA AO REAL, DECIDI COLOCAR PESOS(WEIGHTS) 
+NO STATUS JA QUE NENHUM BANCO CONSEGUE SOBREVIVER SE 1 A CADA 3 TRANSACOES
+FALHAREM. PARA ISSO SETEI UMA VARIAVEL(PESOS) CORRESPONDENTE A CADA TIPO DE STATUS.
+O k DEFINE QUANTOS ELEMENTOS SERAO SORTEADOS OU SEJA, APENAS UM POR REQUISICAO.
+ESSE [0] FOI UM POUCO ESTRANHO DE ENTENDER, CHOICES RETORNA UMA LISTA, UTLIZANDO [0]
+O PYTHON 'PEGA' O VALOR DE DENTRO DA LISTA E ATRIBUI A VARIAVEL STATUS.
+'''
 
 lista_clientes = []
 
